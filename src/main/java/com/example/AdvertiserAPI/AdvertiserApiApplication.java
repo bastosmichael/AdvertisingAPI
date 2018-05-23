823200c73681e00ee4e54e0c1d5110a367f2df42
Michael Bastos <bastosmichael@gmail.com>
2018-05-23 09:46:56 -0500

    Initial Commit

---
+++ src/main/java/com/example/AdvertiserAPI/AdvertiserApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.AdvertiserAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertiserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertiserApiApplication.class, args);
+	}
+}
