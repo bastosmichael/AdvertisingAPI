89be219573164c6565e028bd5925eb2655a55bff
Michael Bastos <bastosmichael@gmail.com>
2018-05-26 17:12:39 -0500

    still building controllers and adding junit

---
+++ src/main/java/advertisers/AdvertisersController.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class AdvertisersController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "{}";
+    }
+
+}
